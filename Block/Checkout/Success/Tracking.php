e7c3035ed19fc0a465e1b59124d74fc7456abb3d
Martin Knudsen <mkk@wexo.dk>
2022-01-21 13:48:13 +0100

    [2936-176] updated module with new featureset based on client wishes

--- Block/Checkout/Success/Tracking.php
+++ Block/Checkout/Success/Tracking.php
@@ -7,6 +7,7 @@ use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Stdlib\CookieManagerInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\Store\Model\ScopeInterface;
+use Psr\Log\LoggerInterface;
 
 class Tracking extends \Magento\Framework\View\Element\Template
 {
@@ -16,12 +17,14 @@ class Tracking extends \Magento\Framework\View\Element\Template
 
     const MAKEINFLUENCE_TRACKING_URL = 'https://system.makeinfluence.com/track-conversion';
     public CookieManagerInterface $cookieManager;
+    private LoggerInterface $logger;
 
     public function __construct(
         ScopeConfigInterface $scopeConfig,
         Session              $checkoutSession,
         RequestInterface     $request,
         CookieManagerInterface $cookieManager,
+        LoggerInterface $logger,
         Template\Context     $context,
         array                $data = []
     )
@@ -31,9 +34,11 @@ class Tracking extends \Magento\Framework\View\Element\Template
         $this->scopeConfig = $scopeConfig;
         $this->request = $request;
         $this->cookieManager = $cookieManager;
+        $this->logger = $logger;
     }
 
     /**
+     * Responsible to preparing data needed to track affiliate orders
      * @return array
      */
     public function prepareTrackingData()
@@ -60,15 +65,26 @@ class Tracking extends \Magento\Framework\View\Element\Template
         ];
     }
 
+    /**
+     * Verifies module is enabled
+     * @return bool
+     */
     public function isEnabled()
     {
         return $this->scopeConfig->getValue(
             'makeinfluence/general/enabled',
             ScopeInterface::SCOPE_STORE
-        );
+        ) === '1';
     }
 
+    /**
+     * @var \Magento\Sales\Model\Order|null
+     */
     public $order = null;
+
+    /**
+     * @return \Magento\Sales\Model\Order
+     */
     public function getOrder()
     {
         if($this->order === null)
@@ -76,24 +92,37 @@ class Tracking extends \Magento\Framework\View\Element\Template
         return $this->order;
     }
 
+    /**
+     * @return string|null
+     */
     public function getOrderId()
     {
         $order = $this->getOrder();
         return $order->getIncrementId();
     }
 
+    /**
+     * @return string
+     */
     public function getOrderValue()
     {
         $order = $this->getOrder();
         return number_format($order->getBaseSubtotalInclTax(), 2, '.', '');
     }
 
+    /**
+     * @return float|string|null
+     */
     public function getCouponCode()
     {
         $order = $this->getOrder();
         return $order->getCouponCode();
     }
 
+    /**
+     * returns BusinessId set via Stores => Configuration => MakeInfluence => Configuration
+     * @return string|null
+     */
     public function getBusinessId()
     {
         return $this->scopeConfig->getValue(
@@ -102,16 +131,45 @@ class Tracking extends \Magento\Framework\View\Element\Template
         );
     }
 
+    /**
+     * Verifies if backend tracking is enabled via Stores => Configuration => MakeInfluence => Configuration
+     * @return bool
+     */
+    public function isS2StrackingEnabled()
+    {
+        return $this->scopeConfig->getValue(
+            'makeinfluence/general/s2s_tracking',
+            ScopeInterface::SCOPE_STORE
+        ) === '1';
+    }
+
+    /**
+     * Verifies if frontend tracking is enabled via Stores => Configuration => MakeInfluence => Configuration
+     * @return bool
+     */
+    public function isFrontendTrackingEnabled()
+    {
+        return $this->scopeConfig->getValue(
+            'makeinfluence/general/frontend_tracking',
+            ScopeInterface::SCOPE_STORE
+        ) === '1';
+    }
+
+    /**
+     * Based on data from \Wexo\MakeInfluence\Block\Checkout\Success\Tracking::prepareTrackingData
+     * Sends a request to Make Influence with tracking information
+     * @param $data
+     * @return void
+     */
     public function submit($data)
     {
-        $client = new \GuzzleHttp\Client();
         try {
+            $client = new \GuzzleHttp\Client();
             $response = $client->request('POST', self::MAKEINFLUENCE_TRACKING_URL, [
                 'json' => $data
             ]);
-            dump($response->getBody()->getContents());
         } catch (GuzzleException $exception) {
-            dump($exception);
+            $this->logger->error('[\Wexo\MakeInfluence\Block\Checkout\Success\Tracking::submit] Exception' . $exception->getMessage());
         }
     }
 }
